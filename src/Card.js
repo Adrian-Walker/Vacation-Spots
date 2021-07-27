4a1abd1a5cb2701afff91fd03cf7a353ce420500
Adrian Walker <awalker3365@gmail.com>
2021-07-27 01:21:45 -0400

    7-27 second

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,27 @@
 import React from "react"
 
-function Card(){
-    return
-}
+let vacationSpots = [
+    {
+        place: "Meridian, Idaho",
+        price: 40,
+        timeToGo: "Spring"
+    }, {
+        place: "Cancun",
+        price: 900,
+        timeToGo: "Winter"
+    }, {
+        place: "China",
+        price: 1200,
+        timeToGo: "Fall"
+    }, {
+        place: "Russia",
+        price: 1100,
+        timeToGo: "Summer"
+    }, {
+        place: "Lebanon",
+        price: 400,
+        timeToGo: "Spring"
+    }
+]
 
-
-export default Card
+export default vacationSpots
