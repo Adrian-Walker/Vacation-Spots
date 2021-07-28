4803bbd5813fad78a4aa31f25aa2a9aea436c8d2
Adrian Walker <awalker3365@gmail.com>
2021-07-27 22:00:19 -0400

    7-27-21 10p

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React from "react"
+
 import Card from "./Card"
+import vacationSpots from "./vacations"
+// import vacations from "./vacations"
 
 function App() {
-    return <div>
-        <h1>Adrian Walker</h1>
-        <Card />
-    </div>
+    const trips = vacationSpots.map(vacations => <Card key ={vacations.id}place={vacations.place} price = {vacations.price} timeToGo={vacations.timeToGo}/>)
+
+    return (
+        <div>
+            {trips}
+        </div>
+    )
 }
 
 export default App
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,13 @@
 import React from "react"
+import vacationSpots from "./vacations"
 
-function Card(vacations) {
+function Card(props) {
     return (
         <div>
-            Lakers
+            <h2>Vacation City: {vacationSpots.place}</h2>
+            <h2>Price: {vacationSpots.price}</h2>
+            <h2>Best Time To Go: {vacationSpots.timeToGo}</h2>
+            <hr></hr>
         </div>
     )
 }
